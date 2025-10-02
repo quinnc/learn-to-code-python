25a07a80b84f967bf4fc87a38edf864b475e6f83
quinnc <q.liesch+git@gmail.com>
2025-10-02 10:18:44 -0600

    Math library practise

---
+++ S2.9-numbers.py
@@ -0,0 +1,8 @@
+import math
+
+print (math.factorial(5))
+print (math.ceil(2.2))
+
+radius = float ( input ("Enter the circle's radius: ") )
+
+print ("A circle of radius", radius, "has a circumference of", round(2*math.pi*radius, 2), "and an area of", round(math.pi*radius**2, 2))
