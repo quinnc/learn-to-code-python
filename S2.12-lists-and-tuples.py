d4ac6f6693b1250d0d37c2b91a163fcc8554fd1d
quinnc <q.liesch+git@gmail.com>
2025-10-02 10:36:20 -0600

    Add name to a list

--- S2.12-lists-and-tuples.py
+++ S2.12-lists-and-tuples.py
@@ -8,3 +8,14 @@ birth_month = birthday.split('-')
 birth_month_num = int(birth_month[1])
 
 print ("You were born in", months[birth_month_num-1])
+
+print ("Now let's add your name to the class list.")
+new_name = input ("PLease enter your first name: ")
+
+names = ['Aaron', 'Belinda', 'Everly', 'Walter']
+
+names.append (new_name)
+
+print ("The class list is now", names);
+
+
