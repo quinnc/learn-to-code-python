293ff7b8063cddf83aef5c28faf80a44b9d83077
quinnc <q.liesch+git@gmail.com>
2025-10-02 10:32:09 -0600

    Move calcs to variables

--- S2.9-numbers.py
+++ S2.9-numbers.py
@@ -3,6 +3,10 @@ import math
 print (math.factorial(5))
 print (math.ceil(2.2))
 
+print ("This program is going to calculate the area and circumference of a circle given the radius.")
 radius = float ( input ("Enter the circle's radius: ") )
 
-print ("A circle of radius", radius, "has a circumference of", round(2*math.pi*radius, 2), "and an area of", round(math.pi*radius**2, 2))
+area = math.pi * ( radius**2 )
+circumference = 2 * math.pi * radius
+
+print ("A circle of radius", radius, "has a circumference of", round(circumference, 2), "and an area of", round(area, 2))
